8c036df2bf97a3808261d1c5e3cea38b253424d6
Kacper Zabłudowski <zabluka_aehit@students.vizja.pl>
2025-11-18 21:01:28 +0100

    feat: add Camera and Gate modules with controllers and services

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,6 +6,11 @@ import { ParkingService } from './parking/parking.service';
 import { PrismaService } from './prisma/prisma.service';
 import { SubscriptionController } from './subscription/subscription.controller';
 import { SubscriptionService } from './subscription/subscription.service';
+import { GateService } from './gate/gate.service';
+import { CameraService } from './camera/camera.service';
+import { CameraController } from './gate/camera/camera.controller';
+import { CameraController } from './camera/camera.controller';
+import { GateController } from './gate/gate.controller';
 
 @Module({
   imports: [],
@@ -13,12 +18,16 @@ import { SubscriptionService } from './subscription/subscription.service';
     ParkingController,
     ParkingLotController,
     SubscriptionController,
+    CameraController,
+    GateController,
   ],
   providers: [
     ParkingService,
     PrismaService,
     ParkingLotService,
     SubscriptionService,
+    GateService,
+    CameraService,
   ],
 })
 export class AppModule {}
