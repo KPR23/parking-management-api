f9d63098160d3b233f964579013eda84d37e4105
Kacper Zabłudowski <zabluka_aehit@students.vizja.pl>
2025-11-18 21:57:43 +0100

    feat: implement GateController and GateService with getStatus method

--- src/gate/gate.controller.ts
+++ src/gate/gate.controller.ts
@@ -1,4 +1,13 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Param, ParseIntPipe } from '@nestjs/common';
+import { Gate } from '@prisma/client';
+import { GateService } from './gate.service';
 
 @Controller('gate')
-export class GateController {}
+export class GateController {
+  constructor(private readonly gateService: GateService) {}
+
+  @Get('status/:id')
+  getStatus(@Param('id', ParseIntPipe) id: number): Promise<Gate> {
+    return this.gateService.getStatus(id);
+  }
+}
--- src/gate/gate.service.ts
+++ src/gate/gate.service.ts
@@ -1,4 +1,19 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { Gate } from '@prisma/client';
+import { PrismaService } from 'src/prisma/prisma.service';
 
 @Injectable()
-export class GateService {}
+export class GateService {
+  constructor(private readonly prisma: PrismaService) {}
+  async getStatus(id: number): Promise<Gate> {
+    const gate = await this.prisma.gate.findUnique({
+      where: { id },
+    });
+
+    if (!gate) {
+      throw new NotFoundException('Gate not found.');
+    }
+
+    return gate;
+  }
+}
